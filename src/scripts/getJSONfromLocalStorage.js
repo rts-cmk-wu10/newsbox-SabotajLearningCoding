f9673e5ffe9ab0ba00284cdc829f1a4a69cba82c
Sabircan <Sabircan@gmail.com>
2023-11-02 13:57:14 +0100

    removed the old files

--- src/scripts/getJSONfromLocalStorage.js
+++
@@ -1,8 +0,0 @@
-function getJSONfromLocalStorage(key) {
-	const OBJECT = localStorage.getItem(key)
-		? JSON.parse(localStorage.getItem(key))
-		: []
-	return OBJECT
-}
-
-export default getJSONfromLocalStorage
