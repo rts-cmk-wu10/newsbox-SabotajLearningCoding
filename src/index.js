e49d0a7e22086344ce2faf5c6cfbeb51d68af7dd
Sabircan <Sabircan@gmail.com>
2023-11-16 11:40:24 +0100

    added all the files that need

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./style.scss";
+import "./scripts/localstorage";
+import "./scripts/darkmode";
+import "./scripts/touch";
+import "./scripts/switch";
+
+import "./images/archive-icon.png";
+import "./images/settings-icon.png";
+import "./images/delete-icon.png";
+import "./images/placeholder.png";
+import "./images/arrow-left-icon.png";
---
+++ src/scripts/darkmode.js
@@ -0,0 +1,32 @@
+export default (function () {
+  if (!window.location.pathname.includes("settings.html")) return; // guard clause
+
+  if (
+    window.matchMedia("(prefers-color-scheme: dark)").matches &&
+    localStorage.getItem("theme") !== ""
+  ) {
+    localStorage.setItem("theme", "darkmode");
+  }
+
+  const CTA_BUTTON = document.querySelector(".darkMode-Btn");
+  CTA_BUTTON.addEventListener("click", clickHandler);
+
+  function clickHandler() {
+    const CLASS_LIST = document.body.classList;
+    CLASS_LIST.toggle("darkmode");
+    localStorage.setItem(
+      "theme",
+      CLASS_LIST.contains("darkmode") ? "darkmode" : ""
+    );
+  }
+
+  if (!localStorage.getItem("theme")) {
+    localStorage.setItem("theme", "");
+  }
+
+  if (localStorage.getItem("theme") === "darkmode") {
+    CTA_BUTTON.checked = true;
+  }
+
+  document.body.classList.add(localStorage.getItem("theme"));
+})();
